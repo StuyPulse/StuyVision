cdfe0d44f370d548262ee02292f4130181225e74
Sam Belliveau <sam.belliveau@gmail.com>
2021-03-03 17:12:34 -0500

    fixed run script

--- run.sh
+++ run.sh
@@ -4,9 +4,9 @@ cd ./src
 
 if [ -x "$(command -v py)" ]; then
     py ./main.py
-elif [ -x "$(command -v py)" ]; then
+elif [ -x "$(command -v python3)" ]; then
     python3 ./main.py 
-elif [ -x "$(command -v py)" ]; then
+elif [ -x "$(command -v python)" ]; then
     python ./main.py
 else 
     echo "No available version of Python was found!"
